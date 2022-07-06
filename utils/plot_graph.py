0c1fa6c58fd90f59a1f7e3fe23f84f1d57110ff6
João V. V. Santos <joaovvsantos@proton.me>
2022-07-06 19:22:25 -0300

    ✨ subindo o projeto

---
+++ utils/plot_graph.py
@@ -0,0 +1,11 @@
+from igraph import *
+
+def plot_graph(name, list_edge, qtd_vertex, list_weight, list_label):
+    g = Graph(directed=False)
+    g.add_vertices(qtd_vertex)
+    g.add_edges(list_edge)
+    g.es["weight"] = list_weight
+    visual_style = {}
+    visual_style["vertex_label"] = list_label
+    visual_style["edge_label"] = list_weight
+    return plot(g, f"{name}.png", **visual_style)
